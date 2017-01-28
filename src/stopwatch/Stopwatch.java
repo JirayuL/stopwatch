025a1f2f1d738939b54ad7ea7552f8404666db03
Jirayu Laungwilawan <jirayu.l@ku.th>
2017-01-28 23:25:29 +0700

    Add SpeedTest and javadoc for Stopwatch but didn't update README.md

--- src/stopwatch/Stopwatch.java
+++ src/stopwatch/Stopwatch.java
@@ -15,11 +15,21 @@ public class Stopwatch {
 	private long startTime;
 	private long stoptTime;
 
+	/**
+	 * Constructor for Stopwatch
+	 */
 	public Stopwatch() {
-
+		this.startTime = 0;
+		this.stoptTime = 0;
 	}
 
-	private double getElapsed() {
+	/**
+	 * Check whether the Stopwatch is stopped or running and return the time
+	 * when call this method.
+	 * 
+	 * @return time of Stopwatch when the user call this method.
+	 */
+	public double getElapsed() {
 		if (this.startTime > this.stoptTime) {
 			return (System.nanoTime() - this.startTime) * NANOSECONDS;
 		} else {
@@ -27,27 +37,48 @@ public class Stopwatch {
 		}
 	}
 
-	private boolean isRunning() {
+	/**
+	 * To check whether the Stopwatch is running or stopped and return the result.
+	 * @return Is Stopwatch running or stopped?
+	 */
+	public boolean isRunning() {
 		if (this.startTime > this.stoptTime) {
 			return true;
 		} else {
 			return false;
 		}
 	}
-
-	private void start() {
+	
+	/**
+	 * To get the time when the Stopwatch start.
+	 */
+	public void start() {
 		this.startTime = System.nanoTime();
 	}
-
-	private void stop() {
+	
+	/**
+	 * To get the time when the Stopwatch stop.
+	 */
+	public void stop() {
 		this.stoptTime = System.nanoTime();
 	}
+	
+	/**
+	 * Do something like work or study.
+	 */
+	public static void doSomething() {
+		System.out.println("Hard working!!!");
+	}
 
+	/**
+	 * Main method to test the Stopwatch object.
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		Stopwatch timer = new Stopwatch();
 		System.out.println("Starting task");
 		timer.start();
-		// doSomething(); // do some work
+		doSomething(); // do some work
 		timer.stop(); // stop timing the work
 		System.out.printf("elapsed = %.6f sec\n", timer.getElapsed());
 		if (timer.isRunning())
